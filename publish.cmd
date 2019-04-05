84b2e902592fd1157429bc31c9daa3bd58a7a247
Daniel Evans <dev@microsoft.com>
2019-04-05 15:56:01 -0700

    Initial commit of code downloaded from Azure portal. Missing PostDeploySripts dir, because secrets.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=devstreamingechotestbot-Web-Deploy.pubxml -p:Password=T4NXhobeduLyu4syrMJiKf02Q4jcbNa8LL0iFm17Hp8ql1241p87MrfwZ71Y
+
