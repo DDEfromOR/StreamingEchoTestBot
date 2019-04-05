74112958e0cadf2bb1ae5d4efb4678aa87ce4292
Daniel Evans <dev@microsoft.com>
2019-04-05 16:33:24 -0700

    Update Startup to setup Streaming Extensions.

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Integration.AspNet.Core.StreamingExtensions;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,6 +34,7 @@ namespace Microsoft.Bot.Builder.EchoBot
 
             // Create the credential provider to be used with the Bot Framework Adapter.
             services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+            services.AddSingleton<BotFrameworkWebSocketAdapter>();
 
             services.AddSingleton<IChannelProvider, ConfigurationChannelProvider>();
 
@@ -57,7 +59,7 @@ namespace Microsoft.Bot.Builder.EchoBot
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
+            app.UseWebSockets();
             //app.UseHttpsRedirection();
             app.UseMvc();
         }
